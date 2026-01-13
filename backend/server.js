69aa50dd9f12ff9d571c5c629bb0929cd10075a0
Mohammed Parves <mdperrfan@gmail.com>
2026-01-13 19:58:23 +0600

    updated

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 3000;
 
 // CORS configuration
 const allowedOrigins = [
-    "https://omm-clothing.vercel.app",
+    "https://ommverse.vercel.app",
     "https://omm-admin.vercel.app",
     "http://localhost:3000",
     "http://localhost:5173",
