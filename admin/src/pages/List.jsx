6a2419e4e058ec15a80846e8d759c50d2b803225
MDPerrfan <mdperrfan@gmail.com>
2026-01-20 13:49:47 +0600

    updated

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -237,7 +237,7 @@ const List = ({ token }) => {
       <p className="mb-2">All Products List</p>
 
       <div className="flex flex-col gap-2">
-        <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr_1fr_1fr_1fr] items-center py-2 px-2 border bg-gray-100 text-sm">
+        <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr_1fr_1fr_1fr] items-center gap-2 py-2 px-2 border bg-gray-100 text-sm">
           <b>Image</b>
           <b>Name</b>
           <b>Category</b>
