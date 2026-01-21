8d1ef11908353402e162b6b729933ca7b24b2559
MDPerrfan <mdperrfan@gmail.com>
2026-01-21 23:56:25 +0600

    Added new link

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://localhost:5174",
+    "https://omm-admin.netlify.app"
 ];
 
 // Connect DB & Cloudinary
